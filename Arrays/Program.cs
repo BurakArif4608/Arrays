12bdbfe73046943dc5ca25f4c7766c56afa1ebf8
PC <PC@BRKPC>
2024-03-19 23:43:37 +0300

    Proje dosyası ekle.

---
+++ Arrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[2];
+           
+            students[0] = "Burak";
+            students[1] = "Hüseyin";
+
+            foreach (var student  in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            string[,] iller = new string[1, 3]
+            {
+                {"istanbul","Ankara" ,"İzmir"}
+
+            };
+
+            foreach (var il in iller)
+            {
+                Console.WriteLine(il);
+            }
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
